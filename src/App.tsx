882b632955f541bc803294ba6174369c874d0f1f
Alexandre Finger <alexandre.finger@esales.com.br>
2020-06-30 13:14:48 -0300

    Fixing GlobalStyles position

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import Header from './components/Header';
 import Board from './components/Board';
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend} >
+    <DndProvider backend={HTML5Backend}>
       <GlobalStyles />
       <Header />
       <Board />
