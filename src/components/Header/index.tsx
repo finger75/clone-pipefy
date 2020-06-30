0470ee2676438f5fc5d418de801846524966f1f6
Alexandre Finger <alexandre.finger@esales.com.br>
2020-06-30 00:17:40 -0300

    Header updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Container } from './styles';
 const Header: React.FC = () => {
     return (
         <Container>
-            <h1>Rocketfy</h1>
+            <h1>Pipefy</h1><h4>developed by Alexandre</h4>
         </Container>
     )
 }
